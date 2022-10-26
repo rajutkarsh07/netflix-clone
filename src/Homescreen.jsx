253dd56e7a2dd1ab58b247f5a9a979674c1ca31b
utkarsh_raj <rajutkarsh2505@gmail.com>
2022-10-26 23:09:13 +0530

    initial commit

---
+++ src/Homescreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Homescreen.css";
+
+const Homescreen = () => {
+  return (
+    <div className="Homescreen">
+      <h2>This is home screen</h2>
+    </div>
+  );
+};
+
+export default Homescreen;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBGSStj5iH1aVfvh8ZSUIX9CoPHmskOW_8",
+  authDomain: "netflix-clone-c3c79.firebaseapp.com",
+  projectId: "netflix-clone-c3c79",
+  storageBucket: "netflix-clone-c3c79.appspot.com",
+  messagingSenderId: "866213245846",
+  appId: "1:866213245846:web:98d480001bd4a9aa2ea3b7",
+  measurementId: "G-FKWBY8LJSP",
+};
